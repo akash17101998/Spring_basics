1c33b4bcd2bc1db799cee72fe38ee5852334b5f2
user <singhakash.101998@gmail.com>
2023-06-01 12:55:30 +0530

    dynamic file upload

--- src/main/java/com/example/spring/demo/controller/FileUploadController.java
+++ src/main/java/com/example/spring/demo/controller/FileUploadController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class FileUploadController{
@@ -31,7 +32,8 @@ public class FileUploadController{
             // file upload
           boolean f=  fileUploadHelper.uploadFile(file);
             if (f){
-                return ResponseEntity.ok("File is uplaoded ");
+//                return ResponseEntity.ok("File is uplaoded ");
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(file.getOriginalFilename()).toUriString());
             }
         }catch (Exception e){
             e.printStackTrace();
