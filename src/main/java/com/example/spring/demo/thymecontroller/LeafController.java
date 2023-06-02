ea4704aa95be68d503e2356f64f258a5f702413f
user <singhakash.101998@gmail.com>
2023-06-02 13:55:47 +0530

    thymeleaf:-iteration

--- src/main/java/com/example/spring/demo/thymecontroller/LeafController.java
+++ src/main/java/com/example/spring/demo/thymecontroller/LeafController.java
@@ -2,10 +2,12 @@ package com.example.spring.demo.thymecontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class LeafController{
@@ -15,5 +17,17 @@ public class LeafController{
         model.addAttribute("name","Akash");
         model.addAttribute("currentDate",new Date().toString());
         return "about";
+        }
+    @GetMapping("/iterate")
+    public String iterateHandle(Model m){
+//        int[] a= new int[]{1,2,3,4,5};
+//        for(int e:a) {
+//            System.out.println(e);
+//
+//        }
+//        m.addAttribute("e",a.toString());
+        List<String> e = List.of("akash","arun","aman");
+        m.addAttribute("e",e);
+        return "iterate";
     }
 }
